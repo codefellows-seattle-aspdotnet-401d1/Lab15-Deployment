0f81154c17b41f12cc966ade7f722c34b54b7741
Brandon Little <brlittle86@gmail.com>
2017-10-02 11:12:50 -0700

    got back to where I was, now struggling with IQueryable things

---
+++ lab15-brandon-try3/SchoolRegistry/SchoolRegistry/ViewModels/StudentCoursesViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using SchoolRegistry.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolRegistry.ViewModels
+{
+    public class StudentCoursesViewModel
+    {
+        public List<Student> Students { get; set; }
+        public SelectList Courses { get; set; }
+
+        public string StudentCourse { get; set; }
+    }
+}
