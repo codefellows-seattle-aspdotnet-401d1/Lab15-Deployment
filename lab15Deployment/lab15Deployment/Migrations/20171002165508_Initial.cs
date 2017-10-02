659e4ef8000cf63101d82c2e4785743c8134fdaa
Gregory Dukes <kcils360@gmail.com>
2017-10-02 12:47:04 -0700

    lab15 completed

---
+++ lab15Deployment/lab15Deployment/Migrations/20171002165508_Initial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace lab15Deployment.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Birthday = table.Column<int>(type: "int", nullable: false),
+                    Course = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
---
+++ lab15Deployment/lab15Deployment/Models/Students.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab15Deployment.Models
+{
+    public class Students
+    {
+        public int ID { get; set; }
+
+        [StringLength(20, MinimumLength = 3)]
+        [Display(Name = "First Name")]
+        [Required]
+        public string FirstName { get; set; }
+
+        [StringLength(20, MinimumLength = 3)]
+        [Display(Name = "Last Name")]
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Course { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Birthday { get; set; }
+    }
+}
