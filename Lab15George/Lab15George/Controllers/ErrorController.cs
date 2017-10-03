97c5b906a89e92be22ce5b01e521a20c768e73a1
George Gaylor <georgegaylor83@outlook.com>
2017-10-02 23:49:28 -0700

    Initial Work

---
+++ Lab15George/Lab15George/Controllers/ErrorController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab15George.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
---
+++ Lab15George/Lab15George/Data/Lab15GeorgeContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab15George.Models
+{
+    public class Lab15GeorgeContext : DbContext
+    {
+        public Lab15GeorgeContext (DbContextOptions<Lab15GeorgeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Lab15George.Models.Register> Register { get; set; }
+    }
+}
